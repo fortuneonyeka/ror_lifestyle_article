f6695b29590f4f445257e57a5a12d1dc403cdab2
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-07-21 17:16:59 +0100

    update config route with article resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :categories
+  # get 'categories/new'
+  # post 'categories/create'
+  # get 'categories/update'
+  # get 'categories/destroy'
+  root to: 'categories#index'
+  # get 'home/index'
+  # root to: "home#index"
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
