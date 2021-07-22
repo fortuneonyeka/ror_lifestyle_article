8ef9a556f72248ede24ea6359e8d8cef09e11e9d
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-07-22 01:34:29 +0100

    remove commented routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :categories
-  # get 'categories/new'
-  # post 'categories/create'
-  # get 'categories/update'
-  # get 'categories/destroy'
+  # 
   # root to: 'articles#index'
   root 'categories#index'
   # get 'home/index'
-  # root to: "home#index"
+  
   resources :articles
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
